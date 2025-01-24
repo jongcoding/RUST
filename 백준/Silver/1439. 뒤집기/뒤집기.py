432b84b39366a7b8728fda32033853190f696719
JONG <128033808+jongcoding@users.noreply.github.com>
2025-01-24 20:09:20 +0900

    [Silver V] Title: 뒤집기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1439. 뒤집기/뒤집기.py
+++ 백준/Silver/1439. 뒤집기/뒤집기.py
@@ -1,23 +1,5 @@
-import sys
-def min_greedy(s):
-    if not s:
-        return 0
-    z =0
-    o=0
-    current=s[0]
-    if current=='0':
-        z+=1
-    else:
-        o+=1
-    for char in s[1:]:
-        if char != current:
-            if char=='0':
-                z +=1
-            else:
-                o +=1
-            current=char
-    return min(z,o)
+s=input()
 
-if __name__ =="__main__":
-    s=sys.stdin.readline().rstrip()
-    print(min_greedy(s))
+one=[o for o in s.split('0') if o !='']
+zero=[o for o in s.split('1') if o !='']
+print(min(len(one),len(zero)))
